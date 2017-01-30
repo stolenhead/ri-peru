9a2682ae6b84b1576b36214fb30b0b272446eca0
stolenhead <andrea.palomino.ruby@gmail.com>
2017-01-29 23:43:03 -0500

    mejora botones , add localizacion

--- style/js/view2.js
+++ style/js/view2.js
@@ -3,13 +3,20 @@ var openNav =function () {
 }
 var closeNav=function () {
     $("#mySidenav").removeClass("open");
-}   
+}  
+var aparece =function () {
+	 $("#location").removeClass("hiden");
+} 
 $(document).ready(function(){
 	$(".menu").click(function(){
 		$(".button-collapse").sideNav();
 	});
 	$(".cross_icon").click(function () {
 		$(".button-collapse").sideNav('hide');
+	})
+	$("#move2").click(function(){
+		 $('#move').modal();
+		 aparece();
 	})
     // $("#modal_info").click(function(){
 	$('#modal_info').modal();
